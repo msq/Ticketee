6aaa7dc108c155efa6b7197e26b88137dfd7a572
Michiel Quellhorst <michielquellhorst@gmail.com>
2015-12-28 11:45:54 +0100

    Added Devise and sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
